faf573646273d60541e3efbdab1d6470cd504a9a
aryanetorquato-ship-it <aryane.torquato@gmail.com>
2026-08-10 19:53:09 +0000

    Testa comando WMI de armazenamento

--- projetos/LogicB0B/TestePowerShell.pas
+++ projetos/LogicB0B/TestePowerShell.pas
@@ -10,7 +10,6 @@ uses
 var
   Processo: TProcess;
   Saida: TStringList;
-
 begin
   Processo := TProcess.Create(nil);
   Saida := TStringList.Create;
@@ -22,7 +21,17 @@ begin
     Processo.Parameters.Add('-NoProfile');
     Processo.Parameters.Add('-NonInteractive');
     Processo.Parameters.Add('-Command');
-    Processo.Parameters.Add('Write-Output "TESTE-LOGICBOB"');
+    Processo.Parameters.Add(
+      'Write-Output "ANTES-WMI"; ' +
+      'Get-WmiObject Win32_LogicalDisk -Filter "DriveType=3" | ' +
+      'ForEach-Object { ' +
+      '$letra = $_.DeviceID; ' +
+      '$total = [math]::Round($_.Size / 1GB, 2); ' +
+      '$livre = [math]::Round($_.FreeSpace / 1GB, 2); ' +
+      'Write-Output "$letra - $total GB total - $livre GB livre" ' +
+      '}; ' +
+      'Write-Output "DEPOIS-WMI"'
+    );
 
     Processo.Options := [poUsePipes, poWaitOnExit];
 
@@ -31,7 +40,6 @@ begin
     WriteLn('3 - Depois de Execute.');
 
     Saida.LoadFromStream(Processo.Output);
-
     WriteLn('4 - Saida recebida:');
     WriteLn(Saida.Text);
 
