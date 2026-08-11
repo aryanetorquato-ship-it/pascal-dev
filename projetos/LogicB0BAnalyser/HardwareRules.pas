bdfe2bd6c9902339e35ffc902dd74b8f3e6de5eb
aryanetorquato-ship-it <aryane.torquato@gmail.com>
2026-08-11 21:05:10 +0000

    Corrige HardwareRules.pas: remove conteudo duplicado/corrompido por heredoc mal fechado

--- projetos/LogicB0BAnalyser/HardwareRules.pas
+++ projetos/LogicB0BAnalyser/HardwareRules.pas
@@ -15,7 +15,7 @@ type
     Motivo: String;
   end;
 
-function ExtrairGeracaoIntel(const NomeCPU: String): Integer; // -1 = nao identificado
+function ExtrairGeracaoIntel(const NomeCPU: String): Integer;
 function EhCeleronDualCoreCompativel(const NomeCPU: String): Boolean;
 function ExtrairRAMGB(const TextoRAM: String): Integer;
 function AvaliarPapel(const Papel, NomeCPU: String; RAMGB: Integer;
@@ -43,7 +43,6 @@ begin
 
   I := Pos1 + 2;
   Digitos := '';
-  // pula espacos e hifen entre "i5" e o numero do modelo
   while (I <= Length(Nome)) and ((Nome[I] = ' ') or (Nome[I] = '-')) do
     Inc(I);
 
@@ -83,112 +82,7 @@ begin
     if Texto[I] in ['0'..'9'] then
       Digitos := Digitos + Texto[I]
     else if Digitos <> '' then
-      Break; // pega so o primeiro bloco numerico (ex: "8 GB" -> "8")
-  end;
-  Result := StrToIntDef(Digitos, 0);
-end;
-
-function AvaliarPapel(const Papel, NomeCPU: String; RAMGB: Integer;
-  const SistemaOperacional: String): TResultadoAvaliacao;
-var
-  Geracao: Integer;
-begin
-  Geracao := ExtrairGeracaoIntel(NomeCPU);
-
-  Result.Nivel := naNaoClassificado;
-  Result.Motivo := 'Nao foi possivel classificar (CPU: ' + NomeCPU + ', RAM: ' +
-    IntToStr(RAMGB) + 'GB)';
-
-  if
-cd /workspaces/pascal-dev/projetos/LogicB0BAnalyser
-
-cat > HardwareRules.pas << 'EOF'
-unit HardwareRules;
-
-{$mode objfpc}{$H+}
-
-interface
-
-uses
-  SysUtils;
-
-type
-  TNivelAdequacao = (naNaoClassificado, naLegado, naMinima, naRecomendada, naIdeal);
-
-  TResultadoAvaliacao = record
-    Nivel: TNivelAdequacao;
-    Motivo: String;
-  end;
-
-function ExtrairGeracaoIntel(const NomeCPU: String): Integer; // -1 = nao identificado
-function EhCeleronDualCoreCompativel(const NomeCPU: String): Boolean;
-function ExtrairRAMGB(const TextoRAM: String): Integer;
-function AvaliarPapel(const Papel, NomeCPU: String; RAMGB: Integer;
-  const SistemaOperacional: String): TResultadoAvaliacao;
-function NivelParaTexto(Nivel: TNivelAdequacao): String;
-
-implementation
-
-function ExtrairGeracaoIntel(const NomeCPU: String): Integer;
-var
-  Nome: String;
-  Pos1, I: Integer;
-  Digitos: String;
-begin
-  Result := -1;
-  Nome := UpperCase(NomeCPU);
-
-  Pos1 := 0;
-  if Pos('I3', Nome) > 0 then Pos1 := Pos('I3', Nome)
-  else if Pos('I5', Nome) > 0 then Pos1 := Pos('I5', Nome)
-  else if Pos('I7', Nome) > 0 then Pos1 := Pos('I7', Nome)
-  else if Pos('I9', Nome) > 0 then Pos1 := Pos('I9', Nome);
-
-  if Pos1 = 0 then Exit;
-
-  I := Pos1 + 2;
-  Digitos := '';
-  // pula espacos e hifen entre "i5" e o numero do modelo
-  while (I <= Length(Nome)) and ((Nome[I] = ' ') or (Nome[I] = '-')) do
-    Inc(I);
-
-  while (I <= Length(Nome)) and (Nome[I] in ['0'..'9']) and (Length(Digitos) < 5) do
-  begin
-    Digitos := Digitos + Nome[I];
-    Inc(I);
-  end;
-
-  if Length(Digitos) = 5 then
-    Result := StrToIntDef(Copy(Digitos, 1, 2), -1)
-  else if Length(Digitos) >= 4 then
-    Result := StrToIntDef(Copy(Digitos, 1, 1), -1);
-end;
-
-function EhCeleronDualCoreCompativel(const NomeCPU: String): Boolean;
-var
-  Nome: String;
-begin
-  Nome := UpperCase(NomeCPU);
-  Result :=
-    (Pos('CELERON', Nome) > 0) and
-    ((Pos('J1800', Nome) > 0) or (Pos('J1900', Nome) > 0) or (Pos('DUAL', Nome) > 0));
-end;
-
-function ExtrairRAMGB(const TextoRAM: String): Integer;
-var
-  Texto: String;
-  I: Integer;
-  Digitos: String;
-begin
-  Result := 0;
-  Texto := UpperCase(TextoRAM);
-  Digitos := '';
-  for I := 1 to Length(Texto) do
-  begin
-    if Texto[I] in ['0'..'9'] then
-      Digitos := Digitos + Texto[I]
-    else if Digitos <> '' then
-      Break; // pega so o primeiro bloco numerico (ex: "8 GB" -> "8")
+      Break;
   end;
   Result := StrToIntDef(Digitos, 0);
 end;
@@ -230,10 +124,10 @@ end;
 function NivelParaTexto(Nivel: TNivelAdequacao): String;
 begin
   case Nivel of
-    naIdeal:            Result := 'Configuracao Ideal';
-    naRecomendada:       Result := 'Configuracao Recomendada';
-    naMinima:            Result := 'Configuracao Minima (legado suportado)';
-    naLegado:            Result := 'Legado';
+    naIdeal:       Result := 'Configuracao Ideal';
+    naRecomendada: Result := 'Configuracao Recomendada';
+    naMinima:      Result := 'Configuracao Minima (legado suportado)';
+    naLegado:      Result := 'Legado';
   else
     Result := 'Nao classificado';
   end;
